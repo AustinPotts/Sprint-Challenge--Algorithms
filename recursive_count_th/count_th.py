53b936d681f01eb86b2d73bbe982633e1ee8fd7d
AustinPotts <pottsevolvements@gmail.com>
2020-06-19 12:48:11 -0400

    Recursive Count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,22 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
+    # step 1. what is our base case? It would either be len of word == 0, since we have no word or it would be 
+    #compare the len of the word to the "th" string so if its just t we still return 0
+    # (the base case) if the length of the word is zero or less than "th" value we are done, we know our answer 
+    # we can't use any loops so what would be ideal? A: If statements 
+     
+    # Base Case 
+    if len(word) == 0 or len(word) < len("th"):
+        return 0
+    
+    #slicing to obtain substring
+    # if the first two letters of word are != to th then remove the first two values from the word recursively 
+    if word[:2] != "th":
+        return count_th(word[1:])
+    # else add 1 then recurse removing the first two values
+    else:
+        return count_th(word[1:]) + 1
+
     
     pass
